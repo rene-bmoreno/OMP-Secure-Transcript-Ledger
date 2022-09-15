6b3da9eadab3a0cc21dc6463a12006901293bafc
rene-bmoreno <112521202+rene-bmoreno@users.noreply.github.com>
2022-09-15 03:26:05 -0400

    Delete chaincode-typescript/src directory

--- chaincode-typescript/src/index.ts
+++
@@ -1,9 +0,0 @@
-/*
- * SPDX-License-Identifier: Apache-2.0
- */
-
-import {TranscriptHandlerContract} from './transcripthandler';
-
-export {TranscriptHandlerContract} from './transcripthandler';
-
-export const contracts: any[] = [TranscriptHandlerContract];
